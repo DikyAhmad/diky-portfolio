bcd0592d136b927d8c8e2a7a24da0181f475f997
Diky <69195698+DikyAhmad@users.noreply.github.com>
2024-03-13 17:04:03 +0700

    first commit

---
+++ assets/js/contact.js
@@ -0,0 +1,14 @@
+function get_linkedin()
+{
+     location.href = "https://www.linkedin.com/in/diky-ahmad/"
+}
+
+function get_gmail()
+{
+    location.href = "https://mail.google.com/mail/?view=cm&source=mailto&to=dikyahmad.k@gmail.com" 
+}
+
+function get_github()
+{
+    location.href = "https://github.com/DikyAhmad"
+}
